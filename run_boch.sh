d927b837f4d9a33c3780931acb3f0b4fb2285cd3
Shahe Ansar <shaheansar123323@gmail.com>
2018-01-04 15:53:37 +0530

    Just cleaned a bunch of deleted files.

--- run_boch.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sudo /sbin/losetup /dev/loop0 floppy.img
-bochs_local
-sudo /sbin/losetup -d /dev/loop0
--- update_image.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sudo losetup /dev/loop0 floppy.img 
-sudo mount /dev/loop0 mnt
-cp test.mk.out mnt/kernel
-sudo umount /dev/loop0
-sudo losetup -d /dev/loop0
